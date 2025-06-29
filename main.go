9906ab32f22ac3e761e0f66246108a820ecb9508
AbhiK57 <112722073+AbhiK57@users.noreply.github.com>
2025-06-28 22:10:06 -0400

    begun implementing client side kafka protocol

--- main.go
+++ main.go
@@ -1,18 +1,34 @@
 package main
 
 import (
-	"fmt"
+	"encoding/binary"
 	"io"
 	"log/slog"
 	"net"
+	"sync"
 )
 
+const (
+	apiKeyProduce int16 = 0
+	apiKeyFetch   int16 = 0
+)
+
+type Message struct {
+	data []byte
+	//..
+}
 type Server struct {
-	ln net.Listener
+	mu          sync.Mutex
+	consOffsets map[string]int
+	buffer      []Message
+	ln          net.Listener
 }
 
 func NewServer() *Server {
-	return &Server{}
+	return &Server{
+		consOffsets: make(map[string]int),
+		buffer:      make([]Message, 0, 1024),
+	}
 }
 
 func (s *Server) Start() error {
@@ -25,21 +41,48 @@ func (s *Server) Listen() error {
 		return err
 	}
 	s.ln = ln
+	slog.Info("Listening on :9092")
+
 	for {
 		connection, err := ln.Accept()
 		if err != nil {
 			if err == io.EOF {
-				return err
+				slog.Error("Error accepting connection", "err", err)
+				continue
 			}
-			slog.Error("Error with server accepting", "err", err)
 		}
 		go s.handleConn(connection)
 	}
 }
 
 func (s *Server) handleConn(conn net.Conn) {
-	fmt.Println("New Connection at ", conn.RemoteAddr())
+	defer conn.Close()
+	slog.Info("New Connection at ", conn.RemoteAddr())
 
+	for {
+		//1. Read the Request (Size: 4 bytes)
+		sizeBytes := make([]byte, 4)
+		if _, err := io.ReadFull(conn, sizeBytes); err != nil {
+			if err == io.EOF {
+				slog.Info("Client closed connection", "remoteAddr", conn.RemoteAddr())
+			} else {
+				slog.Error("Could not read request", "err", err)
+			}
+			return
+		}
+		requestSize := binary.BigEndian.Uint32(sizeBytes)
+
+		requestBytes := make([]byte, requestSize)
+		if _, err := io.ReadFull(conn, requestBytes); err != nil {
+			slog.Error("could not read full request", "err", err)
+			return
+		}
+
+		//apiKey := int16(binary.BigEndian.Uint16(requestBytes[0:2]))
+		//apiVersion := int16(binary.BigEndian.Uint16(requestBytes[2:4]))
+		//correlationID := int32(binary.BigEndian.Uint32(requestBytes[4:8]))
+
+	}
 }
 
 func main() {
